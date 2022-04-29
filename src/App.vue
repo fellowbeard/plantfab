9913798a1eee1d3e48db155e825c1f23e44cc7ae
fellowbeard <virgovibe@gmail.com>
2022-04-28 15:35:28 -1000

    adds plants index

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link>
     |
-    <router-link to="/about">About</router-link>
+    <router-link to="/plants">All Plants</router-link>
+    <!-- <router-link to="/about">About</router-link> -->
   </nav>
   <router-view />
 </template>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,40 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      plants: [],
+    };
+  },
+  created: function () {
+    this.indexPlants();
+  },
+  methods: {
+    indexPlants: function () {
+      axios.get("/plants").then((response) => {
+        console.log("plants index", response);
+        this.plants = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="plants-index">
+    <h1>Plants</h1>
+    <div v-for="plant in plants" v-bind:key="plant.id">
+      <h2>{{ plant.name }}</h2>
+      <img v-bind:src="plant.image_url" v-bind:alt="plant.name" />
+      <h3>{{ plant.description }}</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+img {
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+}
+</style>
